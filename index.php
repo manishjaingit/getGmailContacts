108223168a492631c60b72a620fbdda8ca5110e7
Manish Jain <manishjain1706@gmail.com>
2018-05-31 17:07:33 +0530

    create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<link href="css/style.css" rel="stylesheet" type="text/css"/>
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
+<title>Export Google Contacts Using PHP</title>
+
+</head>
+<body>
+
+<div id="main" class="col-sm-6 col-md-4 col-lg-2">
+<div id="envelope" class="col-sm-6 col-md-4 col-lg-2">
+
+<header id="sign_in" class="col-sm-6 col-md-4 col-lg-2">
+<center>
+<h2>Export Gmail Contacts With Google Oauth PHP</h2>
+</center>
+</header>
+<hr>
+<div id="content" class="col-sm-6 col-md-4 col-lg-2">
+<center>
+<h3 id ="sign">Sign In with Google for retrieving Contacts</h3>
+<div class="col-sm-6 col-md-4 col-lg-2">
+<a href="https://accounts.google.com/o/oauth2/auth?client_id=110811702471-r6sujotgjdsbplc18e2v5i4fvkuitcbp.apps.googleusercontent.com&redirect_uri=http://localhost/test/gmail/callback.php&scope=https://www.google.com/m8/feeds/&response_type=code"><img src="images/sign1.png" alt="" id="signimg"/><div style="width: 100px;
+
+height: 100px;
+
+background-color: aqua;">click to login</div></a>
+</div>
+</center>
+</div>
+</div>
+</div>
+
+</body>
+</html>
