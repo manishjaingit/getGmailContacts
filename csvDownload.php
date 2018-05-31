07a73ca14775165b726d7a154dfbe9a6d2e7dfe8
Manish Jain <manishjain1706@gmail.com>
2018-05-31 17:08:13 +0530

    Create csvDownload.php

---
+++ csvDownload.php
@@ -0,0 +1,24 @@
+<?php
+
+$data = $_POST['email'];
+
+$arry[] = explode(',', $data);
+
+foreach ($arry as $row) {
+	$arlength = count($row);
+	for ($i = 0; $i < $arlength; $i++) {
+		$farry[] = explode(',', $row[$i]);
+	}
+}
+
+header("Content-type: text/csv");
+header("Content-Disposition: attachment; filename=file.csv");
+header("Pragma: no-cache");
+header("Expires: 0");
+
+$file = fopen('php://output', 'w');
+fputcsv($file, array('Description'));
+foreach ($farry as $row) {
+	fputcsv($file, $row);
+}
+exit();
